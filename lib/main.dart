46240e37c1d06d9d029d8aa5f32654b413c55e73
unknown <anthonycordova330@gmail.com>
2022-02-28 19:05:20 -0500

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
  
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
